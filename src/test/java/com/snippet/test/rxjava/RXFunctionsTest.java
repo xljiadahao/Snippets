cd39f412994fcb1de4e9013219f6eecf3be13ea7
xulei <xljiadahao@gmail.com>
2018-06-07 20:06:39 +0800

    RxJava Functions

--- src/test/java/com/snippet/test/rxjava/RXFunctionsTest.java
+++ src/test/java/com/snippet/test/rxjava/RXFunctionsTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import rx.Observable;
 import rx.observers.TestSubscriber;
+import rx.schedulers.Schedulers;
 
 /**
  * 3. RX Marble Diagram: http://rxmarbles.com/
@@ -170,4 +171,133 @@ public class RXFunctionsTest {
         assertThat(testSubscriber.getOnNextEvents()).hasSize(1);
     }
     
+    /**
+     * test RxJava concurrency
+     */
+    
+    /**
+     * 
+     * subscribeOn allows us to set which thread to use for subscription
+     */
+    @Test
+    public void testSubscribeOn() throws Exception {
+        Observable<String> sub = getSubscribeOnObservable();
+        sub.map((s) -> s + Thread.currentThread().getName()
+                .substring(0, Thread.currentThread().getName().indexOf("-")))
+                .doOnNext((s) -> printObjAndTread(s)).subscribe(testSubscriber);
+        Thread.sleep(100);
+        testSubscriber.assertNoErrors();
+        assertThat(testSubscriber.getOnNextEvents())
+                .hasSize(3)
+                .containsOnlyOnce("Hello, world 1!RxIoScheduler")
+                .containsOnlyOnce("Hello, world 2!RxIoScheduler")
+                .containsOnlyOnce("Hello, world 3!RxIoScheduler");
+    }
+    
+    /**
+     * 
+     * observeOn allows us to set which thread to use for the pipeline 
+     */
+    @Test
+    public void testObserveOn() throws Exception {
+        Observable<String> sub = getSubscribeOnObservable();
+        sub.observeOn(Schedulers.computation()).map((s) -> s + Thread.currentThread().getName()
+                .substring(0, Thread.currentThread().getName().indexOf("-")))
+                .doOnNext((s) -> printObjAndTread(s)).subscribe(testSubscriber);
+        Thread.sleep(100);
+        testSubscriber.assertNoErrors();
+        assertThat(testSubscriber.getOnNextEvents())
+                .hasSize(3)
+                .containsOnlyOnce("Hello, world 1!RxComputationScheduler")
+                .containsOnlyOnce("Hello, world 2!RxComputationScheduler")
+                .containsOnlyOnce("Hello, world 3!RxComputationScheduler");
+    }
+    
+     @Test
+    public void testBlocking() throws Exception {
+        Observable<String> sub = getSubscribeOnObservable();
+        sub.observeOn(Schedulers.computation()).map((s) -> "Blocking" + s + Thread.currentThread().getName()
+                .substring(0, Thread.currentThread().getName().indexOf("-")))
+                .doOnNext((s) -> printObjAndTread(s)).toBlocking().subscribe(testSubscriber);
+        testSubscriber.assertNoErrors();
+        assertThat(testSubscriber.getOnNextEvents())
+                .hasSize(5)
+                .containsOnlyOnce("BlockingHello, world 1!RxComputationScheduler")
+                .containsOnlyOnce("BlockingHello, world 2!RxComputationScheduler")
+                .containsOnlyOnce("BlockingHello, world 3!RxComputationScheduler")
+                .containsOnlyOnce("BlockingHello, world 4!RxComputationScheduler")
+                .containsOnlyOnce("BlockingHello, world 5!RxComputationScheduler");
+    }
+    
+    @Test
+    public void testProcessItemsWithSingleThread() {
+        String someStrings[] = {"Alipay", "PayPal", "US", "China", "Singapore"};
+        Observable
+                .from(someStrings)
+                .observeOn(Schedulers.computation())
+                .map(String::toUpperCase)
+                .map(s -> s + " " + Thread.currentThread().getName())  
+                .doOnNext(s -> printMessageAndThread(s)).toBlocking()
+                .subscribe(testSubscriber);
+        testSubscriber.assertNoErrors();
+        String headElement = (String) testSubscriber.getOnNextEvents().get(0);
+        int threadId = Integer.parseInt(headElement.substring(headElement.indexOf("-") + 1));
+        assertThat(testSubscriber.getOnNextEvents())
+                .hasSize(5)
+                .containsOnlyOnce("ALIPAY RxComputationScheduler-" + threadId)
+                .containsOnlyOnce("PAYPAL RxComputationScheduler-" + threadId)
+                .containsOnlyOnce("CHINA RxComputationScheduler-" + threadId)
+                .containsOnlyOnce("US RxComputationScheduler-" + threadId)
+                .containsOnlyOnce("SINGAPORE RxComputationScheduler-" + threadId);
+    }
+    
+    @Test
+    public void testProcessItemsWithMultipleThread() {
+        String someStrings[] = {"Alipay", "PayPal", "US", "China", "Singapore"};
+        Observable
+                .from(someStrings)
+                .flatMap(sentence -> 
+                        Observable.just(sentence)
+                        .subscribeOn(Schedulers.computation())
+                        .map(String::toUpperCase)
+                        .doOnNext((s) -> printObjAndTread(s)))
+                .toList()
+                .observeOn(Schedulers.io())
+                .doOnNext((s) -> printObjAndTread(s))
+                .toBlocking()
+                .subscribe(testSubscriber);
+        assertThat(testSubscriber.getOnNextEvents()).hasSize(1);
+    }
+
+    private static Observable<String> getSubscribeOnObservable() {
+        return Observable.<String>create((sub) -> {
+            sub.onNext("Hello, world 1!");
+            sub.onNext("Hello, world 2!");
+            sub.onNext("Hello, world 3!");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                
+            }
+            sub.onNext("Hello, world 4!");
+            sub.onNext("Hello, world 5!");
+            sub.onCompleted();
+        }).subscribeOn(Schedulers.io()); 
+    }
+
+    public static final <T> T printThread(T obj) {
+        System.out.println("Executing on thread named: " + Thread.currentThread().getName());
+        return obj;
+    }
+    public static final <T> T printObjAndTread(T obj) {
+        System.out.println("Received " + obj + " on thread named " + Thread.currentThread().getName());
+        return obj;
+    }
+    public static final <T extends Throwable> void printExceptionAndTread(T e) {
+        System.out.println("Exception " + e.getMessage() + " on thread named " + Thread.currentThread().getName());
+    }
+    public static final void printMessageAndThread(String msg) {
+        System.out.println("Message: " + msg + " from thread named " + Thread.currentThread().getName());
+    }
+
 }
